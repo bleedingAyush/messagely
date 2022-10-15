1454c8b6f985361f56eae22accb1d36e7524fdc4
Ayush Kumar <kumarayush6543@gmail.com>
2022-10-15 14:42:24 +0530

    Only use websockets in connection

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 const url = process.env.REACT_APP_BASE_URL;
 
-export const socket = io(`${url}`);
+export const socket = io(`${url}`, { transports: ["websocket"] });
